e713b8bda267dbb5e645e2181277aea7e915877a
Gavin Mitchell <gavin.mitchell@btsgroupuk.com>
2022-09-28 17:08:39 +0100

    initial changes v2

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,43 +7,49 @@
 module.exports = {
   /* Your site config here */
   siteMetadata: {
-    title: `Woo - Free Responsive HTML5/CSS3 Template`,
+    title: `mitchMedia - A music, media and technology company`,
     author: {
-      name: `Joe Bloggs`,
+      name: `Gavin Mitchell`,
     },
-    description: `Woo is a clean, modern, landing page site template.`,
+    description: `mitchMedia - A music, media and technology company.`,
 
     /* Choose and arrange the sections to be displayed on the landing page */
     sections: [
       `hero`,
       `features`,
-      `pricing`,
-      `call-to-action`,
-      `screenshots`,
-      `testimonials`,
-      `subscribe`
+      //      `pricing`,
+      //      `call-to-action`,
+      //    `screenshots`,
+      //      `testimonials`,
+      //      `subscribe`
     ],
 
     /* Configure the navigation menu */
     menuItems: [
-      {path: 'features', label: 'Features'},
-      {path: 'pricing', label: 'Pricing'},
-      {path: 'screenshots', label: 'Screenshots'},
-      {path: 'testimonials', label: 'Testimonials'},
-      {path: 'subscribe', label: 'Subscribe'},
+      { path: 'features', label: 'Features' },
+      //      { path: 'pricing', label: 'Pricing' },
+      //      { path: 'screenshots', label: 'Screenshots' },
+      //      { path: 'testimonials', label: 'Testimonials' },
+      //      { path: 'subscribe', label: 'Subscribe' },
     ],
 
     /* Provide social media accounts. Remove or add new services. (The value 'fa' is the name of FontAwesome icon to display. Choose from here: https://fontawesome.com/cheatsheet/free/brands ) */
     social: [
-      { service: `Facebook`,
-        url : `https://facebook.com/styleshout`,
-        fa: `facebook` },
-      { service: `Twitter`,
-        url : `https://twitter.com/styleshout`,
-        fa: `twitter` },
-      { service: `GitHub`,
-        url : `https://github.com/desmukh/gatsby-starter-woo`,
-        fa: `github` },
+      {
+        service: `Facebook`,
+        url: `https://facebook.com/`,
+        fa: `facebook`
+      },
+      {
+        service: `Twitter`,
+        url: `https://twitter.com/`,
+        fa: `twitter`
+      },
+      {
+        service: `Instagram`,
+        url: `https://instagram.com/`,
+        fa: `instagram`
+      },
     ],
 
     /* Path to favicon and logo (located inside the 'static' folder) */
@@ -53,20 +59,20 @@ module.exports = {
     /* Logo and text to show in footer */
     footer: {
       logo: `/images/footer-logo.png`,
-      text: `This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris.`,
+      text: `Some text.`,
     },
 
     /* Address and contact info to show in footer */
     address: {
-      line1: `1600 Amphitheatre Parkway`,
-      line2: `Mountain View, CA`,
-      line3: `94043 US`,
+      line1: ``,
+      line2: ``,
+      line3: ``,
     },
     contacts: [
-      { text: `647.343.8234`, url: `tel:6473438234` },
-      { text: `123.456.7890`, url: `tel:1234567890`},
-      { text: `someone@woosite.com`, url: `mailto:someone@woo.com`},
+      { text: ``, url: `` },
+      { text: ``, url: `` },
+      { text: `Email`, url: `mailto:contact@mitchmedia.co.uk` },
     ],
   },
-  plugins: [ `gatsby-plugin-anchor-links`, `gatsby-plugin-smoothscroll`, `react-scrollspy` ],
+  plugins: [`gatsby-plugin-anchor-links`, `gatsby-plugin-smoothscroll`, `react-scrollspy`],
 }
--- src/components/hero.js
+++ src/components/hero.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { AnchorLink } from "gatsby-plugin-anchor-links"
+/* import { AnchorLink } from "gatsby-plugin-anchor-links" */
 import content from "../../content/hero.yaml"
 
 export default () => (
@@ -8,17 +8,17 @@ export default () => (
       <div className="twelve columns">
 
         <div className="hero-text">
-          <h1 className="responsive-headline">{ content.headline }</h1>
-          <p>{ content.body }</p>
+          <h1 className="responsive-headline">{content.headline}</h1>
+          <p>{content.body}</p>
         </div>
 
-        <div className="buttons">
+        {/*         <div className="buttons">
           <AnchorLink className="button trial animated shake" to={ content.button1.to }>{ content.button1.label }</AnchorLink>
           <AnchorLink className="button learn-more" to={ content.button2.to }>{ content.button2.label }</AnchorLink>
         </div>
-
+ */}
         <div className="hero-image">
-          <img src={ content.image } alt="" className="animated fadeInUpBig" />
+          <img src={content.image} alt="" className="animated fadeInUpBig" />
         </div>
       </div>
     </div>
